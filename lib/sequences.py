814f251e8a7cef89ce5b0b9fa6552a3edffcbd76
Keith Capers <K.Capers94@gmail.com>
2024-09-25 12:49:33 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    new_list = []
+    if length == 0:
+        print(new_list)
+    if length == 1:
+        print([0])
+    if length == 2:
+        print([0,1])
+    if length == 10:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21,34])
